ccb760ae1192da2d52f3ae8a8fb30b4da41af744
Joseph .M 101 <jmig3066@gmail.com>
2021-12-07 21:42:07 -0500

    Code updates

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import platform
 from datetime import datetime, date, time, timedelta
 import time
 
-from PySide2.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QToolButton, QProgressBar, QFrame
+from PySide2.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QToolButton, QProgressBar, QFrame, QMenu, QMessageBox, QAction, QMessageBox
 from PySide2.QtCore import QFile, QTimer, QThread, QRunnable, QThreadPool
 from PySide2.QtUiTools import QUiLoader
 
@@ -70,6 +70,7 @@ OctoprintRequestUrl_Job = OctoprintRequestUrl + "job"
 OctoprintRequestUrl_Server = OctoprintRequestUrl + "server"
 OctoprintRequestUrl_Printer = OctoprintRequestUrl + "printer"
 OctoprintRequestUrl_Version = OctoprintRequestUrl + "version"
+OctoprintRequestUrl_Settings = OctoprintRequestUrl + "settings"
 OctoprintRequestUrl_Connection = OctoprintRequestUrl + "connection"
 OctoprintRequestUrl_Printer_State = OctoprintRequestUrl_Printer + "/state"
 OctoprintRequestUrl_Tool = OctoprintRequestUrl + "printer/tool"
@@ -81,8 +82,12 @@ class Widget(QWidget):
     Request_DefaultTimeoutPeriod = 1 # Seconds
     session = requests.Session()
 
+    actionText_disableHeaters = "Turn heaters off"
+
     def __init__(self):
         super(Widget, self).__init__()
+        self.session.headers.update({'X-Api-Key': ApiKey, 'content-type': 'application/json'})
+
         self.load_ui()
         self.timeLabel = self.findChild(QLabel, 'timeLabel')
         self.dateLabel = self.findChild(QLabel, 'dateLabel')
@@ -96,11 +101,15 @@ class Widget(QWidget):
         self.label_job_timeElapsed = self.findChild(QLabel, 'label_job_timeElapsed')
         self.label_jobName = self.findChild(QLabel, 'label_jobName')
 
-        self.session.headers.update({'X-Api-Key': ApiKey, 'content-type': 'application/json'})
-        self.RefreshData()
+        self.menu = self.findChild(QToolButton, 'toolButton_menu')
+        #self.menu.
+        self.menuItems = QMenu()
+        self.populateMenu()
+
+        self.s_RefreshData()
         # Update time and date now
         self.printTimeAndDate()
-        print(self.GetServerVersion())
+        # print(self.GetServerVersion())
         # Start timer for refreshing time and date
         self.timer_refreshTimeAndDate = QTimer()
         self.timer_refreshTimeAndDate.timeout.connect(self.printTimeAndDate)
@@ -110,6 +119,49 @@ class Widget(QWidget):
         self.timer_refreshData.timeout.connect(self.RefreshData)
         self.timer_refreshData.start(1000)
 
+    def RefreshAll(self):
+        self.RefreshData()
+        self.populateMenu()
+
+    def populateMenu(self):
+        self.menuItems = QMenu()
+        # Create refresh action
+        self.action_refresh = QAction("Refresh", self)
+        self.action_refresh.triggered.connect(self.RefreshAll)
+        self.menuItems.addAction(self.action_refresh)
+        # Add submenu
+        try:
+            submenu = QMenu("Preheat")
+            # List profiles
+            profiles = self.Command_GetDefaultTemperatures()
+            submenu.addAction(self.actionText_disableHeaters)
+            submenu.addSeparator()
+            for profile in profiles["profiles"]:
+                submenu.addAction(profile)
+        except:
+            submenu = QMenu("Preheat")
+            action_unavailable = QAction("Profiles not available", self)
+            action_unavailable.setEnabled(False)
+            submenu.addAction(action_unavailable)
+
+        self.menuItems.addMenu(submenu)
+        # Add listener to detect which item was selected
+        submenu.triggered.connect(self.preheat_menuItemSelected)
+        self.menu.setMenu(self.menuItems)
+
+    def preheat_menuItemSelected(self, action):
+        # Get the selected profile
+        profile = action.text()
+        if(profile != self.actionText_disableHeaters):
+            # Set the temps
+            self.Command_SetTempsFromProfile(profile)
+        else:
+            reply = QMessageBox.question(self, 'Disable heaters', "Are you sure you want to turn off the heaters?", QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+            if reply == QMessageBox.Yes:
+                # Turn off heaters
+                self.Command_SetBedTemp(0)
+                self.Command_SetToolTemp(0)
+            
 
     def load_ui(self):
         loader = QUiLoader()
@@ -133,74 +185,75 @@ class Widget(QWidget):
         self.dateLabel.setText(dateText)
 
     def s_RefreshData(self):
-        if(self.lastOperationReportedError):
-            # self.timer_refreshData.setInterval(self.DefaultTimeoutIntervalAfterError)
-            self.timer_refreshData.stop()
-            self.timer_refreshData.start(self.DefaultTimeoutIntervalAfterError)
-            self.lastOperationReportedError = False
-            return False
-        else:
-            self.timer_refreshData.setInterval(1000)
-            self.RefreshData()
-
-    def RefreshData(self):
+        # if(self.lastOperationReportedError):
+        #     # self.timer_refreshData.setInterval(self.DefaultTimeoutIntervalAfterError)
+        #     self.timer_refreshData.stop()
+        #     self.timer_refreshData.start(self.DefaultTimeoutIntervalAfterError)
+        #     self.lastOperationReportedError = False
+        #     return False
+        # else:
+        #     self.timer_refreshData.setInterval(1000)
+        #     self.RefreshData()
         try:
-            response = self.session.get(OctoprintRequestUrl_Job, timeout=Request_DefaultTimeoutPeriod)
-            if response.status_code != 200:
-                print('HTTP', response.status_code)
-                self.SetToDisconnectedView()
-            else:
-                self.label_connectedStatus.setText("Connected")
-
-                response_connection = self.session.get(OctoprintRequestUrl_Connection, timeout=Request_DefaultTimeoutPeriod)
-                self.label_printerConnectionStatus.setText(response_connection.json()["current"]["state"])
-
-                data = response.json()
-                state = data["state"]
-                if state == "Printing":
-                    #self.label_jobStatus.setText("Printing")
-                    progress = int(data["progress"]["completion"]) * 100
-                    self.printStatus_progressBar.setValue(progress)
-
-                    secondsRemaining = data["progress"]["printTimeLeft"]
-                    secondsElapsed = data["progress"]["printTime"]
-
-                    futureTime = datetime.now() + timedelta(seconds=secondsRemaining)
-
-                    finishTimeAndDate = futureTime.strftime("%m/%d/%Y at %{0}I:%M %p".format(getSeparationChar()))
-                    elapsedTime = time.strftime('%H:%M:%S', time.gmtime(secondsElapsed))
-                    remainingTime = time.strftime('%H:%M:%S', time.gmtime(secondsRemaining))
-
-                    self.label_job_timeElapsed.setText(elapsedTime)
-                    self.label_job_timeRemaining.setText("{0} (on {1})".format(remainingTime, finishTimeAndDate))
-                    self.label_jobName.setText(data["job"]["file"]["name"])
-                    self.jobStatusFrame.setVisible(True)
-                else:
-                    self.jobStatusFrame.setVisible(False)
-
-                self.label_jobStatus.setText(state)
-
-                info_tool_response = self.session.get(OctoprintRequestUrl_Tool, timeout=Request_DefaultTimeoutPeriod)
-                info_bed_response = self.session.get(OctoprintRequestUrl_Bed, timeout=Request_DefaultTimeoutPeriod)
-                tool_temp = TemperatureToString(info_tool_response.json()["tool0"]["actual"], False)
-                bed_temp = TemperatureToString(info_bed_response.json()["bed"]["actual"], False)
-
-                # Check if tool has a target temperature
-                if info_tool_response.json()["tool0"]["target"] != None or info_tool_response.json()["tool0"]["target"] != 0:
-                    tool_target = "(Target: {0})".format(TemperatureToString(info_tool_response.json()["tool0"]["target"], False))
-                else:
-                    tool_target = ""
-
-                # Check if bed has a target temperature
-                if info_bed_response.json()["bed"]["target"] != None or info_bed_response.json()["bed"]["target"] != 0:
-                    bed_target = "(Target: {0})".format(TemperatureToString(info_bed_response.json()["bed"]["target"], False))
-                else:
-                    bed_target = ""
-
-                self.label_printerTemps.setText("Tool: {0} {1} \nBed: {2} {3}".format(tool_temp, tool_target, bed_temp, bed_target))
+            self.RefreshData()
         except:
             print("Connection error")
             self.SetToDisconnectedView()
+
+    def RefreshData(self):
+        response = self.session.get(OctoprintRequestUrl_Job, timeout=self.Request_DefaultTimeoutPeriod)
+        if response.status_code != 200:
+            print('HTTP', response.status_code)
+            self.SetToDisconnectedView()
+        else:
+            self.label_connectedStatus.setText("Connected")
+
+            response_connection = self.session.get(OctoprintRequestUrl_Connection, timeout=self.Request_DefaultTimeoutPeriod)
+            self.label_printerConnectionStatus.setText(response_connection.json()["current"]["state"])
+
+            data = response.json()
+            state = data["state"]
+            if state == "Printing":
+                #self.label_jobStatus.setText("Printing")
+                progress = int(data["progress"]["completion"]) * 100
+                self.printStatus_progressBar.setValue(progress)
+
+                secondsRemaining = data["progress"]["printTimeLeft"]
+                secondsElapsed = data["progress"]["printTime"]
+
+                futureTime = datetime.now() + timedelta(seconds=secondsRemaining)
+
+                finishTimeAndDate = futureTime.strftime("%m/%d/%Y at %{0}I:%M %p".format(getSeparationChar()))
+                elapsedTime = time.strftime('%H:%M:%S', time.gmtime(secondsElapsed))
+                remainingTime = time.strftime('%H:%M:%S', time.gmtime(secondsRemaining))
+
+                self.label_job_timeElapsed.setText(elapsedTime)
+                self.label_job_timeRemaining.setText("{0} (on {1})".format(remainingTime, finishTimeAndDate))
+                self.label_jobName.setText(data["job"]["file"]["name"])
+                self.jobStatusFrame.setVisible(True)
+            else:
+                self.jobStatusFrame.setVisible(False)
+
+            self.label_jobStatus.setText(state)
+
+            info_tool_response = self.session.get(OctoprintRequestUrl_Tool, timeout=self.Request_DefaultTimeoutPeriod)
+            info_bed_response = self.session.get(OctoprintRequestUrl_Bed, timeout=self.Request_DefaultTimeoutPeriod)
+            tool_temp = TemperatureToString(info_tool_response.json()["tool0"]["actual"], False)
+            bed_temp = TemperatureToString(info_bed_response.json()["bed"]["actual"], False)
+
+            # Check if tool has a target temperature
+            if info_tool_response.json()["tool0"]["target"] != None or info_tool_response.json()["tool0"]["target"] != 0:
+                tool_target = "(Target: {0})".format(TemperatureToString(info_tool_response.json()["tool0"]["target"], False))
+            else:
+                tool_target = ""
+
+            # Check if bed has a target temperature
+            if info_bed_response.json()["bed"]["target"] != None or info_bed_response.json()["bed"]["target"] != 0:
+                bed_target = "(Target: {0})".format(TemperatureToString(info_bed_response.json()["bed"]["target"], False))
+            else:
+                bed_target = ""
+
+            self.label_printerTemps.setText("Tool: {0} {1} \nBed: {2} {3}".format(tool_temp, tool_target, bed_temp, bed_target))
         
     
     def SetToDisconnectedView(self):
@@ -212,11 +265,49 @@ class Widget(QWidget):
 
     def GetServerVersion(self):
         # Get Octoprint server version
-        response = self.session.get(OctoprintRequestUrl_Version)
+        response = self.session.get(OctoprintRequestUrl_Version, timeout=self.Request_DefaultTimeoutPeriod)
         if response.status_code != 200:
             return "Error"
         else:
             return response.json()["server"]
+
+    def Command_SetToolTemp(self, tool_temp_c):
+        request = {'command': 'target', 'targets': {'tool0': tool_temp_c}}
+        response = self.session.post(OctoprintRequestUrl_Tool, json=request, timeout=self.Request_DefaultTimeoutPeriod)
+        print(response)
+        return response
+
+    def Command_SetBedTemp(self, bed_temp_c):
+        request = {'command': 'target', 'target': bed_temp_c}
+        response = self.session.post(OctoprintRequestUrl_Bed, json=request, timeout=self.Request_DefaultTimeoutPeriod)
+        print(response)
+        return response
+
+    # Get default profiles & temperatures
+    def Command_GetDefaultTemperatures(self):
+        response = self.session.get(OctoprintRequestUrl_Settings, timeout=self.Request_DefaultTimeoutPeriod)
+        profiles = {}
+        print(response.json())
+        profileList = response.json()["temperature"]["profiles"]
+        for profile in profileList:
+            profiles.update({profile["name"]: {"tool": profile["extruder"], "bed": profile["bed"]}})
+
+        root = {"profiles": profiles}
+        return root
+
+    def Command_SetTempsFromProfile(self, profileName):
+        profiles = self.Command_GetDefaultTemperatures()
+        if profileName in profiles["profiles"]:
+            tool_temp = profiles["profiles"][profileName]["tool"]
+            bed_temp = profiles["profiles"][profileName]["bed"]
+            # Ask user for confirmation
+            reply = QMessageBox.question(self, 'Preheat', "Set preheat profile to \"" + profileName + "\". \nTool: "+TemperatureToString(tool_temp, 0) + "\nBed: "+TemperatureToString(bed_temp, 0)+"\n\nAre you sure you want to apply the profile now?", QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+            if reply == QMessageBox.Yes:
+                self.Command_SetToolTemp(tool_temp)
+                self.Command_SetBedTemp(bed_temp)
+            return True
+        else:
+            return False
 
 if __name__ == "__main__":
     app = QApplication([])
---
+++ run.sh
@@ -0,0 +1,12 @@
+install_pyside2()
+{
+    # Install PySide2
+    printf "Could not find PySide2 libraries for Python 3\nInstalling PySide2\n"
+    sudo apt-get install python3-pyside2.qt3dcore python3-pyside2.qt3dinput python3-pyside2.qt3dlogic python3-pyside2.qt3drender python3-pyside2.qtcharts python3-pyside2.qtconcurrent python3-pyside2.qtcore python3-pyside2.qtgui python3-pyside2.qthelp python3-pyside2.qtlocation python3-pyside2.qtmultimedia python3-pyside2.qtmultimediawidgets python3-pyside2.qtnetwork python3-pyside2.qtopengl python3-pyside2.qtpositioning python3-pyside2.qtprintsupport python3-pyside2.qtqml python3-pyside2.qtquick python3-pyside2.qtquickwidgets python3-pyside2.qtscript python3-pyside2.qtscripttools python3-pyside2.qtsensors python3-pyside2.qtsql python3-pyside2.qtsvg python3-pyside2.qttest python3-pyside2.qttexttospeech python3-pyside2.qtuitools python3-pyside2.qtwebchannel python3-pyside2.qtwebsockets python3-pyside2.qtwidgets python3-pyside2.qtx11extras python3-pyside2.qtxml python3-pyside2.qtxmlpatterns python3-pyside2uic -y
+}
+
+# Check if PySide2 needs to be installed
+python3 -m pyside2 |& grep -q 'No module named' && install_pyside2
+
+# Run
+python3 main.py
